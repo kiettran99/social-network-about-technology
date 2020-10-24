14b722726a023a5de1ed9a0d8b49767181b139ea
Kiet Tran <17110165@student.hcmute.edu.vn>
2020-10-24 16:13:22 +0700

    Add More Endpoints Group
    - Thêm các endpoint của group về upload wallpaper, thay đổi avatar.

--- API/src/routers/api/group.js
+++ API/src/routers/api/group.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const { body, validationResult } = require('express-validator');
+const upload = require('../../utils/upload');
+const storage = require('../../firebase/firebase');
 const Group = require('../../models/group');
 const authByRole = require('../../middleware/auth-by-role');
 
@@ -47,8 +49,9 @@ router.get('/:id', async (req, res) => {
 // @route POST /api/groups/:id
 // @desc Create a group.
 // @access private Admin
-router.post('/', authByRole('admin'), [
-    body('name').not().isEmpty()
+router.post('/', authByRole('admin'), upload.single('avatar'), [
+    body('name', 'Name is required.').not().isEmpty(),
+    body('info', 'Info is required.').not().isEmpty()
 ], async (req, res) => {
     try {
         const errors = validationResult(req);
@@ -61,7 +64,108 @@ router.post('/', authByRole('admin'), [
         // Create a new instance and save into database
         const group = new Group(req.body);
 
-        group.save();
+        await group.save();
+
+        if (req.file) {
+
+            // Create a storage ref
+            const storageRef = storage.ref(`/groups/${group.id}/${req.file.originalname}`);
+
+            // Upload Image
+            await storageRef.put(req.file.buffer);
+
+            group.avatar = await storageRef.getDownloadURL();
+
+            await group.save();
+        }
+
+        res.json(group);
+    }
+    catch (e) {
+        console.log(e);
+        res.status(500).send('Server is errors.');
+    }
+});
+
+// @route PUT /api/groups/:id/name
+// @desc update profile a group.
+// @access private Admin
+router.put('/:id', authByRole('admin'), async (req, res) => {
+    try {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const id = req.params.id;
+        const { name, info } = req.body;
+
+        // Check id is not empty
+        if (!id) {
+            return res.status(400).send('Group id is empty.');
+        }
+
+        const group = await Group.findByIdAndUpdate(id, {
+            name,
+            info
+        });
+
+        res.json(group);
+    }
+    catch (e) {
+        console.log(e);
+        res.status(500).send('Server is errors.');
+    }
+});
+
+// @route PUT /api/groups/:id/avatar
+// @desc upload avatar
+// @access private Admin
+router.put('/:id/avatar', authByRole('admin'), async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        // Check id is not empty
+        if (!id) {
+            return res.status(400).send('Group id is empty.');
+        }
+
+        const group = await Group.findById(id);
+
+        // Check image is uploaded
+        if (req.file) {
+            const prevStorageRef = storage.ref(`/groups/${group.id}`);
+
+            // Remove image just uploaded before
+            prevStorageRef.listAll().then((res) => {
+
+            });
+        }
+        
+        res.json(group);
+    }
+    catch (e) {
+        console.log(e);
+        res.status(500).send('Server is errors.');
+    }
+});
+
+// @route POST /api/groups/:id
+// @desc Delete a group.
+// @access private Admin
+router.delete('/:id', authByRole('admin'), async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        // Check id is not empty
+        if (!id) {
+            return res.status(400).send('Group id is empty.');
+        }
+
+        const group = await Group.findById(id);
+
+        await group.remove();
 
         res.json(group);
     }
