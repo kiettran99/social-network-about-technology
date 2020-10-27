8f514067d4f98cfdee52c064a677d8a24aaab467
Kiet Tran <17110165@student.hcmute.edu.vn>
2020-10-27 16:06:15 +0700

    Change Interface Not Found Component

--- APP/src/components/not-found-page/NotFoundPage.js
+++ APP/src/components/not-found-page/NotFoundPage.js
@@ -1,34 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NotFoundPage = () => (
-    <div className='m-auto container' style={{ width: "80%" }}>
+const NotFoundPage = () => {
+    return (
+        <div id="content-page" className="content-page" >
+            <div className="wrapper mt-5">
+                <div className="mt-5 iq-maintenance">
+                    <div className="container-fluid p-0">
+                        <div className="row no-gutters">
+                            <div className="col-sm-12 text-center">
+                                <div className="iq-maintenance">
+                                    <img src="images/error/02.png" className="img-fluid w-50" alt="" />
+                                    <div className='ma-4'>
+                                        <h1 className='text-primary'>404 NOT FOUND</h1>
+                                    </div>
 
-        <div className='row not-found-row'>
-            <div className='col'>
-                <img src={"https://thumbs.dreamstime.com/b/bot-icon-chatbot-cute-outline-robot-vector-flat-line-cartoon-illustration-isolated-white-background-voice-support-service-123217587.jpg"}
-                    alt="robot=not-found"
-                    style={{ height: '18.75rem' }} />
-            </div>
+                                    <div className='ma-4'>
+                                        <p className='text-muted'>
+                                            We looked everywhere for this page. Are you sure the website URL is correct?.
+                                            Get in touch with the site owner.
+                                        </p>
 
-            <div className='col not-found-col'>
-                <div className='ma-4'>
-                    <h1 className='text-primary'>404 NOT FOUND</h1>
+                                    <Link to='/' className='btn btn-outline-primary'>Go Back Home</Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <div className='ma-4'>
-                    <p className='text-primary'>
-                        We looked everywhere for this page. Are you sure the website URL is correct?.
-                        Get in touch with the site owner.
-                </p>
-                </div>
-
-                <Link to='/' className='btn btn-outline-primary'>Go Back Home</Link>
             </div>
         </div>
-
-
-    </div>
-);
+    );
+};
 
 export default NotFoundPage;
