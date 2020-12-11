9f238e32da29ee4f6f87686c6fad5fca437c2482
nguyendat1234 <47876667+nguyendat1234@users.noreply.github.com>
2020-12-11 11:03:50 +0700

    fix-login

--- Admin/src/App.js
+++ Admin/src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
       <HashRouter>
           <React.Suspense fallback={loading}>
             <Switch>
-              <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
+              <Route exact path="/" name="Login Page" render={props => <Login {...props}/>} />
               <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
               <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
               <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
